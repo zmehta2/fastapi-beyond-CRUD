f7d3731870bfab4a4e0946c6e8f9f94f6c24f31f
zmehta2 <zmehta2@dons.usfca.edu>
2025-02-17 16:21:29 -0800

    bug fixing

--- src/db/main.py
+++ src/db/main.py
@@ -5,18 +5,14 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.config import Config
 
-async_engine = AsyncEngine(create_engine(url=Config.DATABASE_URL))
-
+async_engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
 
 async def init_db() -> None:
     async with async_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
 
-
 async def get_session() -> AsyncSession:
-    Session = sessionmaker(
-        bind=async_engine, class_=AsyncSession, expire_on_commit=False
-    )
+    Session = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
     async with Session() as session:
         yield session
